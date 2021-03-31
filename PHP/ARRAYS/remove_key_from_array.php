03d6da4b7715f57413336c9383152fac2c1aaa47
Andreas <13046122+Ifnir@users.noreply.github.com>
2021-03-31 10:24:10 +0200

    Update remove_key_from_array.php

--- PHP/ARRAYS/remove_key_from_array.php
+++ PHP/ARRAYS/remove_key_from_array.php
@@ -8,4 +8,26 @@ $dynamicFields = ["TYPE" => "simple", "CONFIGURABLE_ATTRIBUTES" => ""];
 * returns []
 *
 */
+
 return array_diff_key($dynamicFields, array_flip($excludeField));
+
+
+/************************************************************************/
+$example1 = 
+[
+  "size" => 
+    [
+      "en" => "L"
+    ]
+];
+
+array_map(function ($value) {
+  return $value['en'];
+}, $example1);
+
+/*
+*
+* https://www.php.net/manual/en/function.array-map.php
+*
+* return ["size" => "L"]
+*/
